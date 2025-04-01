4945839dd751b4898c2418d8bf17298f611f526a
LuxJakob <lux.jakob@gmail.com>
2025-04-01 13:36:12 +0200

    changed setup

--- src/main.py
+++ src/main.py
@@ -1,32 +1,29 @@
 from typing import List
 
+TEST_INPUT_01 = "III"
+TEST_INPUT_02 = "LVIII"
+TEST_INPUT_03 = "MCMXCIV"
+TEST_INPUT_04 = "4"
+
+EXPECTED_OUTPUT_01 = 3
+EXPECTED_OUTPUT_02 = 58
+EXPECTED_OUTPUT_03 = 1994
+EXPECTED_OUTPUT_04 = 0
+
 
 class LeetCode:
-    def run(self, strs: List[str]) -> str:
-        prefix = ""
-        if not strs:
-            return prefix
-        i = 0
+    def run(self, s: str) -> int:
+        integer_number = 0
 
-        strs.sort()
-        for letter in strs[0]:
-            current_letter = letter
-            for word in strs:
-                if current_letter != word[i]:
-                    return prefix
-            prefix = prefix + current_letter
-            i = i + 1
 
-        return prefix  # No solution found
+        return integer_number
 
 
 if __name__ == '__main__':
-    argument_01 = (
-        ["flower", "flower"]
-    )
-    argument_02 = (
-        ["dog","racecar","car"]
-    )
+    test_inputs = [TEST_INPUT_01, TEST_INPUT_02, TEST_INPUT_03, TEST_INPUT_04]
+    expected_outputs = [EXPECTED_OUTPUT_01, EXPECTED_OUTPUT_02, EXPECTED_OUTPUT_03, EXPECTED_OUTPUT_04]
     leet_code_tester = LeetCode()
-    print(f'Your Input: {argument_02}')
-    print(f'Your Output: {leet_code_tester.run(argument_02)}')
+
+    for i in range(len(test_inputs) - 1):
+        print(f'Your Input: {test_inputs[i]} - Expected Output: {expected_outputs[i]}')
+        print(f'Your Output: {leet_code_tester.run(test_inputs[i])}')
--- tests/testcases.py
+++ tests/testcases.py
@@ -1,27 +1,18 @@
 import unittest
 from parameterized import parameterized
-from src.main import LeetCode
+from src.main import *
 
 
 class TestMyFunction(unittest.TestCase):
-
-    test_input_01 = ["flower","flow","flight"]
-    expected_output_01 = "fl"
-    test_input_02 = ["dog","racecar","car"]
-    expected_output_02 = ""
-    test_input_03 = [-1,1,-6,4,5,-6,1,4,1]
-    expected_output_03 = [5,-1,4,4,-6,-6,1,1,1]
-    test_input_04 = [1,5,0,5]
-    expected_output_04 = [1,0,5,5]
 
     def setUp(self):
         self.instance = LeetCode()
 
     @parameterized.expand([
-        (test_input_01, expected_output_01),
-        (test_input_02, expected_output_02),
-        #(test_input_03, expected_output_03),
-        #(test_input_04, expected_output_04),
+        (TEST_INPUT_01, EXPECTED_OUTPUT_01),
+        (TEST_INPUT_02, EXPECTED_OUTPUT_02),
+        (TEST_INPUT_03, EXPECTED_OUTPUT_03),
+        #(TEST_INPUT_04, EXPECTED_OUTPUT_04),
     ])
     def test_run(self, test_input, expected_output):
         result = self.instance.run(test_input)
